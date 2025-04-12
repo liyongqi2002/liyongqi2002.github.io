b0dca48f05a6d1b4b082a2c4c538ec96c4ca1e17
NLPWM-WHU <nlpwm_whu@163.com>
2025-04-12 13:23:13 +0800

    commit

--- assets/javascripts/my.js
+++
@@ -1,8 +0,0 @@
-// // 使用JavaScript动态调整文本框的高度以匹配图片的高度
-// window.addEventListener("load", function () {
-//     var image = document.getElementById("myImage");
-//     var text = document.getElementById("myText");
-    
-//     // 设置文本框的高度等于图片的高度
-//     text.style.height = image.clientHeight + "px";
-// });
